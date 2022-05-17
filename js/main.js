d450545a4ebe8b0f98234c752a12ada8a87ccd89
Zachary Chapman-Cueco <chapmancueco@gmail.com>
2022-05-17 16:57:40 -0400

    Added more sound effects, still missing some

--- js/main.js
+++ js/main.js
@@ -1,20 +1,45 @@
-const sounds = ['Feather Fall']
+//THIS v IS A LOT CLEANER BUT IT MEANS I WILL HAVE TO CHANGE THE WAY I DID MY BUTTONS... WILL FIX LATER
+//=================================================================
+// const sounds = ['Feather_Fall']
 
-sounds.forEach((sound)=> {
-  const btn = document.createElement('button')
-  btn.classList.add('btn')
+// sounds.forEach((sound)=> {
+//   const btn = document.createElement('button')
+//   // btn.classList.add('btn')
 
-  btn.innerText = sound
+//   btn.innerText = sound
 
-  btn.addEventListener('click', ()=>{
-    document.getElementById(sound).play()
-  })
+//   btn.addEventListener('click', ()=>{
+//     document.getElementById(sound).play()
+//   })
 
-  document.getElementById('buttons').appendChild(btn)
-})
+//   document.getElementById('buttons').appendChild(btn)
+// })
+//===================================================================
 
-// document.querySelector('button').addEventListener('click', sound)
+//OW!
+document.querySelector('#ouch').addEventListener('click', playOuch)
 
-// function sound(){
-//   document.getElementById('buttons').play()
-// }
+function playOuch(){
+  document.getElementById('ow').play()
+}
+//============
+//Feather Fall
+document.querySelector('#featherFall').addEventListener('click', playfeatherFall)
+
+function playfeatherFall(){
+  document.getElementById('Feather_Fall').play()
+}
+//===============
+//Tense/Heartbeat
+document.querySelector('#tense').addEventListener('click', playTense)
+
+function playTense(){
+  document.getElementById('heartbeat').play()
+}
+//=========
+//Got Item!
+document.querySelector('#gotItem').addEventListener('click', playGotItem)
+
+function playGotItem(){
+  document.getElementById('zeldaSound').play()
+}
