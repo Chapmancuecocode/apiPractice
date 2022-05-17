824761c9eeb4f93a83ffef0c1714a383ce77ee49
Zachary Chapman-Cueco <chapmancueco@gmail.com>
2022-05-16 20:58:08 -0400

    second version of dnd soundboard, sound working, but array is messing with buttons

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-//alert("works")
+// const sounds = ['fallingFeather']
 
-document.querySelector('#search').addEventListener('click', search)
+// sounds.forEach((sound)=> {
+//   const btn = document.createElement('button')
+//   btn.classList.add('btn')
 
-function search(){
-    let fName = document.querySelector('.fName').value
-    let lName = document.querySelector('.lName').value
+//   btn.innerText = sound
 
-    fetch(`https://wizard-world-api.herokuapp.com/Wizards?FirstName=${fName}&LastName=${lName}`)
-        .then(res => res.json()) // parse response as JSON
-        .then(data => {
-          console.log(data)
-          console.log(fName, lName)
-          document.querySelector('h2').innerText = `1. ${data[0].elixirs[0].name}
-          2. ${data[0].elixirs[1].name}`
-        })
-        .catch(err => {
-            console.log(`error ${err}`)
-    });
-}
+//   btn.addEventListener('click', ()=>{
+//     document.getElementById(sound).play()
+//   })
+
+//   document.getElementById('buttons').appendChild(btn)
+// })
+
+// document.querySelector('button').addEventListener('click', sound)
+
+// function sound(){
+//   document.getElementById('buttons').play()
+// }
