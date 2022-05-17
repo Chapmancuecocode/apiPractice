c8419629b8ea75d84fd3bab232506dc3d67372f6
Zachary Chapman-Cueco <chapmancueco@gmail.com>
2022-05-16 21:13:38 -0400

    modified buttons, sound works, need to add everything else and do some formatting

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-// const sounds = ['fallingFeather']
+const sounds = ['Feather Fall']
 
-// sounds.forEach((sound)=> {
-//   const btn = document.createElement('button')
-//   btn.classList.add('btn')
+sounds.forEach((sound)=> {
+  const btn = document.createElement('button')
+  btn.classList.add('btn')
 
-//   btn.innerText = sound
+  btn.innerText = sound
 
-//   btn.addEventListener('click', ()=>{
-//     document.getElementById(sound).play()
-//   })
+  btn.addEventListener('click', ()=>{
+    document.getElementById(sound).play()
+  })
 
-//   document.getElementById('buttons').appendChild(btn)
-// })
+  document.getElementById('buttons').appendChild(btn)
+})
 
 // document.querySelector('button').addEventListener('click', sound)
 
